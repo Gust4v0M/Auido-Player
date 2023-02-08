39fb4e3808beb08ade6c615fd5d15d24478e5999
Gust4v0M <gustavomorgado6@gmail.com>
2023-02-08 08:37:16 -0300

    seek bar complete

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 let audio = document.getElementById("audio1");
 let audioBar = document.getElementById("audioBar");
 
-let teste = document.getElementById("teste")
+audio.addEventListener("timeupdate", function(){
+  audioBar.value = audio.currentTime / audio.duration
 
-function currentBar() {
+})
 
-  teste.value = audio.currentTime
+
+audioBar.addEventListener("click", atuliza)
+
+function atuliza(e){
+
+let percentage = e.offsetX / this.offsetWidth;
+audio.currentTime = percentage * audio.duration
+ 
+}
+
+function timer(){
+console.log(audio.currentTime)
 }
 
 function volta_5() {
@@ -21,4 +33,5 @@ function pause() {
   audio.pause();
 }
 
-currentBar();
+
+timer()
