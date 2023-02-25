00d212038f029f88605b7af193dfcc9ff28e5247
Gust4v0M <gustavomorgado6@gmail.com>
2023-02-25 14:09:33 -0300

    doing style

--- script.js
+++ script.js
@@ -1,51 +1,54 @@
 let audio = document.getElementById("audio1");
 let audioBar = document.getElementById("audioBar");
-let Ctimer = document.getElementById("Ctimer")
-let Atimer = document.getElementById("Atimer")
- 
-
-audio.addEventListener("timeupdate", function(){
-  audioBar.value = audio.currentTime / audio.duration
-
-})
+let Ctimer = document.getElementById("Ctimer");
+let Atimer = document.getElementById("Atimer");
 
 
-audioBar.addEventListener("click", atuliza)
+audio.addEventListener("timeupdate", function () {
+  audioBar.value = audio.currentTime / audio.duration;
+});
 
-function atuliza(e){
+audioBar.addEventListener("click", atuliza);
 
-let percentage = e.offsetX / this.offsetWidth;
-audio.currentTime = percentage * audio.duration;
-console.log(percentage);
- 
+
+function atuliza(e) {
+  let percentage = e.offsetX / this.offsetWidth;
+  audio.currentTime = percentage * audio.duration;
+  console.log(percentage);
 }
 
-var musicTime  = setInterval(function(){
 
+var musicTime = setInterval(function () {
   var min = Math.floor(audio.currentTime / 60);
   var sec = Math.floor(audio.currentTime % 60);
-    if (sec < 10){
-      sec = "0" + String(sec);
-    }
-
-  Ctimer.innerHTML = min + ':' + sec
-
-
-}, 1)
-
-function allMusicTime(){
-
-
-  let min = Math.floor(audio.duration / 60)
-  let sec = Math.floor(audio.duration % 60)
-
-  if(sec < 10){
-    sec = '0' + String(sec)
+  if (sec < 10) {
+    sec = "0" + String(sec);
   }
 
-  Atimer.innerHTML =  min + ':' + sec  
+  Ctimer.innerHTML = min + ":" + sec;
+}, 10);
 
+
+var allMusicTime = setInterval(function() {
+  let min = Math.floor(audio.duration / 60);
+  let sec = Math.floor(audio.duration % 60);
+
+  if (sec < 10) {
+    sec = "0" + String(sec);
+  }
+
+  Atimer.innerHTML = min + ":" + sec;
+},10)
+
+function nextMusic(){
+
+console.log("teste")
 }
+
+let playlist = {audio : "./assets/Bruno_Mars_-_When_I_Was_Your_Man.mp3",
+                Name : "When i was your men"
+              
+              }
 
 
 
@@ -63,4 +66,4 @@ function pause() {
   audio.pause();
 }
 
-allMusicTime()
+
